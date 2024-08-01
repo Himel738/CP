6d293390cb5cfcedfeab8dd10151aa7541a4486d
Shohanur Rahman Shohan <sohanurhimel@gmail.com>
2024-08-01 20:48:04 +0600

    Create check_if_it_power_of_2_4th_method.cpp

---
+++ Ultimate_Topic_list/Bit_Manipulation/check_if_it_power_of_2_4th_method.cpp
@@ -0,0 +1,32 @@
+
+/* given a value check if its power of 2 means 8 = 2^3.
+   1st approach:
+   if you divided the number by 2 until it reached 1.if it reached 1 then its divisable else not
+   2nd approach:
+   counting by set bit if the set bit count is 1 then its divisable else not
+   3rd approach:
+   by right shifting the value of x until we find value 1 else not.
+   4 th approach:
+   log operation
+   (log2(N)) ceil and floor value will be same
+   else not divisable
+*/
+#include<bits/stdc++.h>
+using namespace std;
+bool ispower(int x)
+{
+    if(x==0)
+    {
+        return false;
+    }
+    return floor(log2(x))==ceil(log2(x)) ? true:false;
+}
+int main ()
+{
+    bool y;
+    int x;
+    cin >> x;
+    y = ispower(x);
+
+    y==true ? cout << "Power of 2"<< endl : cout << "Not Power of 2" << endl;
+}
