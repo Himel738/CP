f809f8a3a5085254a92bcc10b469e6643dfb0245
Shohanur Rahman Shohan <sohanurhimel@gmail.com>
2024-08-01 16:32:32 +0600

    ok

---
+++ Ultimate_Topic_list/Bit_Manipulation/odd_or_even.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int x;
+    cin >> x;
+
+    if((x & 1) == 0)
+    {
+        cout << "Even" << endl;
+    }
+    else
+    {
+        cout << "Odd" << endl;
+    }
+}
