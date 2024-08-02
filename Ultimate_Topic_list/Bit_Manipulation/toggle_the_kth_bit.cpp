98010a08aff0f6a16caae8770c5f209072c243f9
Shohanur Rahman Shohan <sohanurhimel@gmail.com>
2024-08-02 16:08:17 +0600

    Create toggle_the_kth_bit.cpp

---
+++ Ultimate_Topic_list/Bit_Manipulation/toggle_the_kth_bit.cpp
@@ -0,0 +1,13 @@
+/* toggle means if the bit is 1 the convert into 0 */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+    int x,k;
+    cin >> x >> k;
+
+    cout << (x & (1 << k)) << endl;
+
+}
